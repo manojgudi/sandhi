ab5938cd18511c59099cbed9ff3899a5c8381684
manojgudi <manoj.p.gudi@gmail.com>
2015-09-06 08:32:00 +0530

    Integrate scigen block changes and modified codebase to re-build properly with travis-ci
    Fix : travis-ci.yaml file to remove llvm/clang if found
        : Reduce make threads flag
        lib/CMakeLists.txt remove clang/llvm search
    
    Refactored scigen block
        modified: qa_scigen.py
        Updated scigen.xml
        renamed:    generic.py -> scigen.py
    
    Cleanups: removed new creation of scilab instance for every run of work function
    This follows p4 4 spaces standards
    Minimal vimrc http://pastebin.com/RdKLCK1Y

--- modules/gr36/gr-scigen/python/qa_scigen.py
+++ modules/gr36/gr-scigen/python/qa_scigen.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
-# 
-# Copyright 2013 <+YOU OR YOUR COMPANY+>.
-# 
+"""
+
+    Copyright 2015
+    FOSSEE, IIT Bombay
+    Use is subject to license terms.
+
+    @version    0.2, 15-Aug-2015
+    @author     Manoj G
+    @email      manoj.p.gudi@gmail.com
+
+    Test Module to check scilab's generic block
+"""
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
@@ -17,13 +26,15 @@
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
 # 
+import numpy
 
 from gnuradio import gr, gr_unittest
-#import mymod_swig as mymod
-from generic import *
-
+from scigen import Generic
 
 class qa_generic (gr_unittest.TestCase):
+    '''
+    Block to test Generic class from scigen module
+    '''
 
     def setUp (self):
         self.tb = gr.top_block ()
@@ -32,27 +43,28 @@ class qa_generic (gr_unittest.TestCase):
         self.tb = None
 
     def test_001_t (self):
-    	
-    	src0 = numpy.arange(0,3.1416,1)
+        src0 = numpy.arange(0,3.1416,1)
 
-	src0 = gr.vector_source_f(src0)
-	expected_result = (0.0, 0.8414709568023682, 0.9092974066734314, 0.14112000167369843)
+        src0 = gr.vector_source_f(src0)
+        expected_result = (0.0, 0.8414709568023682, 0.9092974066734314, 0.14112000167369843)
 
-	sqr = generic()
-	sqr.set_parameters("sin",1)
+        gen_instance = Generic("sin", 1)
 
-	dst = gr.vector_sink_f()
-	
-	self.tb.connect(src0, (sqr,0)) # src0(vector_source) -> sqr_input_0
-	self.tb.connect(sqr,dst) # sqr_output_0 -> dst (vector_source)
 
-	self.tb.run()
+        dst = gr.vector_sink_f()
 
-	result_data = dst.data()
-	print result_data, "Result data"
-	
-   	self.assertFloatTuplesAlmostEqual(expected_result, result_data, 6)
-	
+
+        self.tb.connect(src0, (gen_instance,0)) # src0(vector_source) -> gen_instance_input_0
+        self.tb.connect(gen_instance,dst) # gen_instance_output_0 -> dst (vector_source)
+
+        self.tb.run()
+
+        result_data = dst.data()
+        print result_data, "Result data"
+
+        self.assertFloatTuplesAlmostEqual(expected_result, result_data, 6)
+
 
 if __name__ == '__main__':
     gr_unittest.main()
+
--- modules/gr36/gr-scigen/python/scigen.py
+++ modules/gr36/gr-scigen/python/scigen.py
@@ -1,3 +1,87 @@
-#!/usr/bin/python
-# Can import other python files
-from generic import * 
+"""
+
+    Copyright 2015
+    FOSSEE, IIT Bombay
+    Use is subject to license terms.
+
+    @version    0.2, 15-Aug-2015
+    @author     Manoj G
+    @email      manoj.p.gudi@gmail.com
+
+    A generic block to call scilab functions which uses sciscipy wrapper
+"""
+import gras
+import numpy
+
+from scilab import Scilab
+
+class Generic(gras.Block):
+    '''
+    Scilab Generic class
+        - inherits gras.Block
+        - used to call scilab functions
+        window_size is size of inputs the function will be called
+    '''
+    def __init__(self, func_name, window_size):
+        gras.Block.__init__(self,
+            name="generic",
+            in_sig=[numpy.float32],
+            out_sig=[numpy.float32])
+
+        self.window_size     = window_size
+        self.func_name       = func_name
+        self.scilab_instance = Scilab()
+
+    def is_window_integral(self, input_item, window):
+        """
+        Check if value of window is integral of length of input source vector
+        For cases like -> input = [3 , 4, 5 ,6] & window = 3
+        """
+        if (len(input_item) % window ):
+            raise Exception("Value of Window should be an integral value of length of input items")
+
+    def work(self, input_items, output_items):
+
+        # Limit output_items to just the size of window
+        output_items[0][:] = output_items[0][:self.window_size]
+
+        # Check number of input_instances
+        n_input_items = len(input_items)
+
+        # Create output string instance which will be evaluated
+        out_eval_string = 'eval("self.scilab_instance.'+self.func_name+'('
+
+        # Iterate for n_input_items
+        for i in range(n_input_items):
+
+            # Check window condition
+            self.is_window_integral(input_items[i][:], self.window_size)
+
+            # If the window is greater than 1, 
+            # input_items[i][:self.window_size] looks like [1   2   3   4   5] which is err for python since it requires comma as delimiters 
+            if self.window_size == 1:
+                """
+                The hell is going on here?
+                """
+                out_eval_string = out_eval_string + str(input_items[i][:self.window_size]) + ","
+            else:
+                print 'IN',str(input_items[i][:self.window_size])
+                out_eval_string = out_eval_string + (str(input_items[i][:self.window_size].tolist()))  + ","  # Replace 10spaces with a singe comma
+
+        out_eval_string = out_eval_string.rstrip(",") + ')")'
+        print "From Scilab",str(out_eval_string)
+
+        # for functions like sin
+        if n_input_items == 1 and self.window_size == 1:
+            output_items[0][:self.window_size] = eval(out_eval_string)
+        else:
+            output_items[0] = eval(out_eval_string)
+
+        print "OUT ",output_items[0]
+
+        #Write a for loop for n_inputs
+        for i in range(n_input_items):
+            self.consume(i,self.window_size) # Consume from port 0 input_items
+
+        self.produce(0,self.window_size) # Produce from port 0 output_items
+
